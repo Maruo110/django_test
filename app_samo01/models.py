565c017969161e2680fee3d342c83817b2308105
Maruo110 <32796229+Maruo110@users.noreply.github.com>
2020-04-25 13:05:22 +0900

    とりあえず「Book」モデルを作成できた

--- app_samo01/models.py
+++ app_samo01/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    # id = AutoField(primary_key=True)  # 自動的に追加されるので定義不要
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    # id = AutoField(primary_key=True)  # 自動的に追加されるので定義不要
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
